4271d73f3efb2e3fbf187895a00b03557b103499
ankitapal189 <59860147+ankitapal189@users.noreply.github.com>
2020-02-06 21:40:39 -0800

    CLASS ACTIVITY 4

---
+++ ext_file.R
@@ -0,0 +1,17 @@
+#----starwars-plot
+starwars %>% 
+  filter(species == "Human") %>%
+  ggplot() +
+  aes(x=height,
+      y=mass) +
+  geom_point()
+
+
+#----starwars-tbl
+starwars %>% 
+  filter(species == "Human") %>%
+  select(name,
+         height,
+         mass,
+         homeworld) %>%
+  knitr::kable()
